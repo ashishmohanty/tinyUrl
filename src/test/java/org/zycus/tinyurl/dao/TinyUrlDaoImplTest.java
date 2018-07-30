1c71dbd506715ca416bbf53754a09c710a6fd834
ashishmohanty <ashish.giet@gmail.com>
2018-07-30 11:41:24 +0530

    Recent Changes

---
+++ src/test/java/org/zycus/tinyurl/dao/TinyUrlDaoImplTest.java
@@ -0,0 +1,23 @@
+package org.zycus.tinyurl.dao;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TinyUrlDaoImplTest {
+	@InjectMocks
+	private TinyUrlDaoImpl tinyUrlDaoImpl;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testCreateTinyUrl() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
